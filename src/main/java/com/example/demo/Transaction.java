15b18cb5cba5a0bc7a77f0ee49461887ff77a00c
firedot1 <zcheng1@montgomerycollege.edu>
2020-06-08 17:01:20 -0400

    first commit

---
+++ src/main/java/com/example/demo/Transaction.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="transaction_db")
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name="amount")
+    private String amount;
+
+    @Column(name="account_id")
+
+
+
+}
